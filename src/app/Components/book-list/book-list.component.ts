56318cdd48234c540c2292b1dc6be2ae78171648
Touqeer <touqeerulhussnain@gmail.com>
2024-07-08 20:11:38 +0500

    added the form Ui

--- src/app/Components/book-list/book-list.component.ts
+++ src/app/Components/book-list/book-list.component.ts
@@ -1,25 +1,44 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Book } from '../../Model/Book';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-book-list',
   standalone: true,
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, HttpClientModule],
   templateUrl: './book-list.component.html',
   styleUrl: './book-list.component.scss'
 })
-export class BookListComponent {
-  books: string[] = [
-    "a", "b", "c"
+export class BookListComponent implements OnInit {
+  books: Book[] = [
+
   ];
 
-  /**
-   *
-   */
-  constructor() {
+  constructor(private http: HttpClient) {
 
 
+  }
+  async ngOnInit() {
+    console.log("Just calling the ngOnInit")
+    this.http.get('https://localhost:7163/api/Book')
+
+      .subscribe(response => {
+
+        console.log(response);
+
+      });
+    // this.books = await this.getContacts();
+    console.log(this.books);
+  }
+
+  private async getContacts(): Promise<any> {
+    console.log("getContacts called");
+    var result = this.http.get("https://localhost:7163/api/Book");
+    console.log(result);
+    return await firstValueFrom(result);
   }
 
 }
--- src/app/Model/Book.ts
+++ src/app/Model/Book.ts
@@ -3,11 +3,14 @@ import { Chapter } from "./Chapter";
 
 
 export interface Book {
+    Id: string,
     Title: string,
     AuthorName: string,
+    PublicationDate: string,
     ISBN: string,
     Genre: string,
-    Tag: [Tag],
-    Chapter: [Chapter]
+    Tag: [],
+    Chapter: []
+    PageCount: number,
 
 }
